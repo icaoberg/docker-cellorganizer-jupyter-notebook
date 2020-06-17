eacbc4aa6cfcaa69f7975b3f415295de96533d99
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2020-06-17 01:30:38 -0400

    Updated scripts

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 docker push icaoberg/cellorganizer-jupyter:latest
 docker push icaoberg/cellorganizer-jupyter:v2.9.0
--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ ! -d data ]; then
+if [ ! -d ~/Desktop/mmbios2020 ]; then
         DIRECTORY=$(pwd)
 
         ####################################################################################################
 	# icaoberg - this creates temporary folder on Desktop and then downloads images from Murphy Lab site
-	mkdir -p ~/Desktop/data/images
-	cd ~/Desktop/data/images
+	mkdir -p ~/Desktop/mmbios2020/images
+	cd ~/Desktop/mmbios2020/images
 
 	#wget -nc http://murphylab.web.cmu.edu/data/Hela/3D/multitiff/cellorganizer_full_image_collection.zip
 	#unzip cellorganizer_full_image_collection.zip
@@ -18,7 +18,7 @@ fi
 
 ############################################################################################################
 docker run --rm -p 8888:8888 \
-	-v ~/Desktop/data:/home/murphylab/cellorganizer/local \
+	-v ~/Desktop/mmbios2020:/home/murphylab/cellorganizer/local \
 	--memory="4g" \
 	--cpus=2 \
 	-e JUPYTER_LAB_ENABLE=yes \

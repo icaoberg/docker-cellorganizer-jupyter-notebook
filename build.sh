9207577a7e3ee16efdda5b067c1549b517dd9292
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2020-06-17 01:10:24 -0400

    Updated build script

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+if [ ! -d cellorganizer-python ]; then
+	mkdir cellorganizer-python
+fi
 git submodule init
 git submodule update
 cd cellorganizer-python
 git checkout master
 cd ..
+
 docker build --no-cache -t icaoberg/cellorganizer-jupyter:latest .
-#docker tag icaoberg/cellorganizer-jupyter:latest icaoberg/cellorganizer-jupyter:v2.9.0
+docker tag icaoberg/cellorganizer-jupyter:latest icaoberg/cellorganizer-jupyter:v2.9.0

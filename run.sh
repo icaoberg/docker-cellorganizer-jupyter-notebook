5b33fbae51c8c95a4d3e73b7a048b49a79de1deb
icaoberg <icaoberg@andrew.cmu.edu>
2020-06-19 14:56:29 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,10 +7,6 @@ if [ ! -d ~/Desktop/mmbios2020 ]; then
 	# icaoberg - this creates temporary folder on Desktop and then downloads images from Murphy Lab site
 	mkdir -p ~/Desktop/mmbios2020/images
 	cd ~/Desktop/mmbios2020/images
-
-	#wget -nc http://murphylab.web.cmu.edu/data/Hela/3D/multitiff/cellorganizer_full_image_collection.zip
-	#unzip cellorganizer_full_image_collection.zip
-	#rm -fv cellorganizer_full_image_collection.zip
         ####################################################################################################
 
 	cd $DIRECTORY
